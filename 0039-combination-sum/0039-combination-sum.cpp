4cc664cc8c9bd1f74d7495e9acd8326d6e77d2fb
Aryan Kamal <76702642+Aryan-kamal@users.noreply.github.com>
2024-12-28 00:15:21 +0530

    Time: 43 ms (17.64%), Space: 45.1 MB (13.55%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -2,17 +2,33 @@ class Solution {
 public:
     void function(vector<int>& candidates, int target,int n,vector<vector<int>> &ans,
                     vector<int>temp,int ind){
-        if(ind==n || target<0)
+        // M-1 -
+        // if(ind==n || target<0)
+        // {
+        //     if(target==0)
+        //         ans.push_back(temp);
+        //     return;
+        // }
+        // target-=candidates[ind];
+        // temp.push_back(candidates[ind]);
+        // function(candidates,target,n,ans,temp,ind);
+        // target+=candidates[ind];
+        // temp.pop_back();
+        // function(candidates,target,n,ans,temp,ind+1);
+        
+        // M-2 (little more effective) -
+        if(ind==n)
         {
             if(target==0)
                 ans.push_back(temp);
             return;
         }
-        target-=candidates[ind];
-        temp.push_back(candidates[ind]);
-        function(candidates,target,n,ans,temp,ind);
-        target+=candidates[ind];
-        temp.pop_back();
+        if(candidates[ind]<=target)
+        {
+            temp.push_back(candidates[ind]);
+            function(candidates,target-candidates[ind],n,ans,temp,ind);
+            temp.pop_back();
+        }
         function(candidates,target,n,ans,temp,ind+1);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
