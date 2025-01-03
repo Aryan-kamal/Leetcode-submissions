89771e0c8953e1ded5cc7efd2352bf3faab155aa
Aryan Kamal <76702642+Aryan-kamal@users.noreply.github.com>
2025-01-03 17:24:14 +0530

    Time: 0 ms (100%), Space: 10.9 MB (38.48%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,23 +1,49 @@
 class Solution {
 public:
-    void function (int ind,vector<int>& temp,vector<int>& nums,int n,set<vector<int>>&ans){
-        if(ind>=n)
+    // M-1 -
+    void function(vector<int>& nums, int n,vector<vector<int>> &ans,
+                    vector<int>temp,int ind){
+        // if(ind == n)
+        //     return;  // base condn not required !
+        ans.push_back(temp);
+        for(int i=ind;i<n;i++)
         {
-            ans.insert(temp);
-            return;
+            if(i == ind || nums[i]!=nums[i-1])
+            {
+                temp.push_back(nums[i]);
+                function(nums,n,ans,temp,i+1);
+                temp.pop_back();
+            }
         }
-        temp.push_back(nums[ind]);
-        function(ind+1,temp,nums,n,ans);
-        temp.pop_back();
-        function(ind+1,temp,nums,n,ans);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>>ans;
+        vector<vector<int>>ans;
         int n=nums.size();
         vector<int> temp;
         sort(nums.begin(),nums.end());
-        function(0,temp,nums,n,ans);
-        vector<vector<int>>ans1(ans.begin(),ans.end());
-        return ans1;
+        function(nums,n,ans,temp,0);
+        return ans;
     }
+
+    // M-2 
+    // void function (int ind,vector<int>& temp,vector<int>& nums,int n,set<vector<int>>&ans){
+    //     if(ind>=n)
+    //     {
+    //         ans.insert(temp);
+    //         return;
+    //     }
+    //     temp.push_back(nums[ind]);
+    //     function(ind+1,temp,nums,n,ans);
+    //     temp.pop_back();
+    //     function(ind+1,temp,nums,n,ans);
+    // }
+    // vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+    //     set<vector<int>>ans;
+    //     int n=nums.size();
+    //     vector<int> temp;
+    //     sort(nums.begin(),nums.end());
+    //     function(0,temp,nums,n,ans);
+    //     vector<vector<int>>ans1(ans.begin(),ans.end());
+    //     return ans1;
+    // }
 };
