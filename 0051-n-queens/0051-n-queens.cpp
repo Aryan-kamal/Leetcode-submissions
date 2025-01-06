14e9faf343112efc254abcb1d8c8ebc64ef3421a
Aryan Kamal <76702642+Aryan-kamal@users.noreply.github.com>
2025-01-07 03:25:13 +0530

    Time: 2 ms (76.22%), Space: 10.3 MB (50.99%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,18 +1,45 @@
 class Solution {
 public:
-    bool isSafe(vector<string>& board, int row, int col, int n) {
-        for (int j = 0; j < col; j++) // Check left row
-            if (board[row][j] == 'Q') 
-                return false;
-        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) // Check upper left diagonal
-            if (board[i][j] == 'Q')
-                return false;
-        for (int i = row, j = col; i < n && j >= 0; i++, j--) // Check lower left diagonal
-            if (board[i][j] == 'Q')
-                return false;
-        return true;
-    }
-    void function(int n,vector<vector<string>>&ans,vector<string>&tempBoard,int j){
+    //M-1 -
+    // bool isSafe(vector<string>& board, int row, int col, int n) {
+    //     for (int j = 0; j < col; j++) // Check left row
+    //         if (board[row][j] == 'Q') 
+    //             return false;
+    //     for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) // Check upper left diagonal
+    //         if (board[i][j] == 'Q')
+    //             return false;
+    //     for (int i = row, j = col; i < n && j >= 0; i++, j--) // Check lower left diagonal
+    //         if (board[i][j] == 'Q')
+    //             return false;
+    //     return true;
+    // }
+    // void function(int n,vector<vector<string>>&ans,vector<string>&tempBoard,int j){
+    //     if(j==n)
+    //     {   
+    //         ans.push_back(tempBoard);
+    //         return;
+    //     }
+    //     for(int x=0;x<n;x++)
+    //     {
+    //         if(isSafe(tempBoard,x,j,n))
+    //         {
+    //             tempBoard[x][j]='Q';
+    //             function(n,ans,tempBoard,j+1);
+    //             tempBoard[x][j]='.';
+    //         }
+    //     }
+            
+    // }
+    // vector<vector<string>> solveNQueens(int n) {
+    //     vector<vector<string>>ans;
+    //     string dotString(n, '.');
+    //     vector<string> tempBoard(n, dotString); //fill the tempBoard with dots
+    //     function(n,ans,tempBoard,0);
+    //     return ans;
+    // }
+
+    // M-2 (optimised one) -
+    void function(int n,vector<vector<string>>&ans,vector<string>&tempBoard,int j,vector<int>&straight,vector<int>&lowerDiag,vector<int>&upperDiag){
         if(j==n)
         {   
             ans.push_back(tempBoard);
@@ -20,11 +47,17 @@ public:
         }
         for(int x=0;x<n;x++)
         {
-            if(isSafe(tempBoard,x,j,n))
+            if(straight[x]!=1 && lowerDiag[x+j]!=1 && upperDiag[n-1+j-x]!=1)
             {
                 tempBoard[x][j]='Q';
-                function(n,ans,tempBoard,j+1);
+                straight[x]=1;
+                lowerDiag[x+j]=1;
+                upperDiag[n-1+j-x]=1;
+                function(n,ans,tempBoard,j+1,straight,lowerDiag,upperDiag);
                 tempBoard[x][j]='.';
+                straight[x]=0;
+                lowerDiag[x+j]=0;
+                upperDiag[n-1+j-x]=0;
             }
         }
             
@@ -33,7 +66,10 @@ public:
         vector<vector<string>>ans;
         string dotString(n, '.');
         vector<string> tempBoard(n, dotString); //fill the tempBoard with dots
-        function(n,ans,tempBoard,0);
+        vector<int>straight(n,0);
+        vector<int>lowerDiag(2*n-1,0);
+        vector<int>upperDiag(2*n-1,0);
+        function(n,ans,tempBoard,0,straight,lowerDiag,upperDiag);
         return ans;
     }
 };
