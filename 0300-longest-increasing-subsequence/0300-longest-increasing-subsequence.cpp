57788b2f21429b225db3dbfac2479e8694e0a379
Aryan Kamal <76702642+Aryan-kamal@users.noreply.github.com>
2025-03-31 20:40:50 +0530

    Time: 63 ms (56.89%), Space: 14.4 MB (45.01%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
-    int f(int ind, int prevInd, vector<int>& nums, vector<vector<int>>& dp) {
-        if (ind == nums.size())
-            return 0;
-        if (dp[ind][prevInd + 1] != -1)
-            return dp[ind][prevInd + 1];
-        int take = 0;
-        int nottake = f(ind + 1, prevInd, nums, dp); // not take
-        if (prevInd == -1 || nums[ind] > nums[prevInd])
-            take = 1 + f(ind + 1, ind, nums, dp); // take
-        return dp[ind][prevInd + 1] = max(nottake, take);
-    }
+    // int f(int ind, int prevInd, vector<int>& nums, vector<vector<int>>& dp) {
+    //     if (ind == nums.size())
+    //         return 0;
+    //     if (dp[ind][prevInd + 1] != -1)
+    //         return dp[ind][prevInd + 1];
+    //     int take = 0;
+    //     int nottake = f(ind + 1, prevInd, nums, dp); // not take
+    //     if (prevInd == -1 || nums[ind] > nums[prevInd])
+    //         take = 1 + f(ind + 1, ind, nums, dp); // take
+    //     return dp[ind][prevInd + 1] = max(nottake, take);
+    // }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return f(0, -1, nums, dp);
+
+        // M - 1 -
+        // vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        // return f(0, -1, nums, dp);
+
+        // M - 2 (optimised tabulation) -
+        vector<int> dp(n,1);
+        int maxInd=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j] && dp[j]+1 > dp[i]){
+                    dp[i]=dp[j]+1;
+                }
+            }
+            if(dp[i]>dp[maxInd]) // or maxi = max(maxi,dp[i])  then return maxi 
+                maxInd=i;
+        }
+        return dp[maxInd];
     }
 };
