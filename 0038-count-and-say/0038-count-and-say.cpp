02584ee80cbea2f4fdf87392c96c5a7bce89fa85
Aryan Kamal <76702642+Aryan-kamal@users.noreply.github.com>
2025-04-18 15:30:40 +0530

    Time: 63 ms (5.37%), Space: 125.7 MB (5.11%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -20,6 +20,29 @@ public:
         return rec(a+1,wd,n);
     }
     string countAndSay(int n) {
+        // M-1 (Recursion)
         return rec(1,"1",n);
     }
 };
+
+// M-2 -
+// string countAndSay(int n) {
+//     if (n == 1) return "1";
+//     string temp = "1";
+//     for (int i = 2; i <= n; i++) {
+//         string curr = "";
+//         int count = 1;
+//         for (int j = 1; j <= temp.size(); j++) {
+//             if (j < temp.size() && temp[j] == temp[j - 1])
+//                 count++;
+//             else {
+//                 curr += to_string(count);
+//                 curr += temp[j - 1];
+//                 count = 1;
+//             }
+//         }
+
+//         temp = curr;
+//     }
+//     return temp;
+// }
